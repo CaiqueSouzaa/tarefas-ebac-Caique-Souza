f09f0a8b64513ca363b2be5d96951e03c6360efe
CaiqueSouzaa <caique.souzaa903@gmail.com>
2024-05-18 13:55:29 -0300

    Fim da atividade módulo 10

--- mod10/src/Main.java
+++ mod10/src/Main.java
@@ -2,6 +2,7 @@ import br.com.csouza.Aplicacao;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.out.println("ATENÇÃO! Utilizar somente [.] para definir as notas dos alunos!");
         Aplicacao app = new Aplicacao();
         app.init();
     }
--- mod10/src/br/com/csouza/Aplicacao.java
+++ mod10/src/br/com/csouza/Aplicacao.java
@@ -1,36 +1,95 @@
 package br.com.csouza;
 
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Aplicacao {
+    private Escola escola;
+    private int periodoLetivo = 0;
+    private int quantAlunos = 0;
+    private ArrayList<Aluno> alunos = new ArrayList<>();
+
     public void init() throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        boolean execStatus = true;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        Escola escola = this.criarEscola(scanner);
-        int periodoLetivo = this.definirPeriodoLetivo(scanner);
-        int quantAlunos = this.quantAlunos(scanner);
-        ArrayList<Aluno> alunos = this.criarAlunos(quantAlunos, periodoLetivo, scanner);
+        // Obrigatório definir o nome da escola e quantia de periodos letivos no inicio da execução
+        this.criarEscola(scanner);
+        this.definirPeriodoLetivo(scanner);
 
-        for (Aluno aluno : alunos) {
-            System.out.println(aluno.notas().getNotasMediaTotal());
+        while (execStatus) {
+            int opcao = this.garantirOpcaoValida(scanner);
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("__________________________\n>> Relatório escolar");
+                    if (!this.alunos.isEmpty()) {
+                        System.out.println(this.escola.relatorioAlunos());
+                    } else {
+                        System.out.printf(">> Não há alunos registrados na escola %s.\n", this.escola.getNome());
+                    }
+                    break;
+
+                case 2:
+                    this.criarAlunos(1, this.periodoLetivo, scanner);
+                    break;
+
+                case 3:
+                    execStatus = false;
+                    System.out.println("Fim de execução.");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
         }
 
         scanner.close();
     }
 
-    private Escola criarEscola(Scanner scanner) {
-        System.out.print("Defina o nome da escola: ");
-        String nome = scanner.nextLine();
-        return new Escola(nome);
+    public void opcoes() {
+        System.out.println("__________________________\n1 - Exibir relatório escolar\n2 - Adicionar novo aluno\n3 - Finalizar a aplicação\n__________________________");
+        System.out.print(">> ");
     }
 
-    private int definirPeriodoLetivo(Scanner scanner){
+    public int garantirOpcaoValida(Scanner scanner) {
+        boolean status = false;
+        int opcao = 0;
+
+        while (!status) {
+            try {
+                this.opcoes();
+                opcao = scanner.nextInt();
+
+                if (opcao < 1 || opcao > 3) {
+                    System.out.println("O valor da opção deve ser somente '1', '2' ou '3'.");
+                } else {
+                    status = true;
+                }
+
+                scanner.nextLine();
+            } catch (Exception err) {
+                System.out.println("Input inválido. Digite somente números inteiros.");
+                scanner.next();
+            }
+        }
+
+        return opcao;
+    }
+
+    private void criarEscola(Scanner scanner) {
+        System.out.print("Defina o nome da escola: ");
+        String nome = scanner.nextLine();
+        this.escola = new Escola(nome);
+    }
+
+    private void definirPeriodoLetivo(Scanner scanner){
         boolean sucesso = false;
         int periodoLetivo = 0;
         while (!sucesso) {
             try {
-                System.out.print("Defina o periodo letivo: ");
+                System.out.print("Defina a quantia de periodos letivos: ");
                 periodoLetivo = scanner.nextInt();
                 if (periodoLetivo <= 0) {
                     System.out.println("O periodo letivo não pode ser inferior ou igual a zero. Tente novamente.");
@@ -43,10 +102,10 @@ public class Aplicacao {
             }
         }
 
-        return periodoLetivo;
+        this.periodoLetivo = periodoLetivo;
     }
 
-    private int quantAlunos(Scanner scanner) {
+    private void definirAlunos(Scanner scanner) {
         boolean sucesso = false;
         int quantAlunos = 0;
 
@@ -65,21 +124,20 @@ public class Aplicacao {
                 scanner.next();
             }
         }
-        return quantAlunos;
+        this.quantAlunos = quantAlunos;
     }
 
-    private ArrayList<Aluno> criarAlunos(int quantAlunos, int periodoLetivo, Scanner scanner) {
-        ArrayList<Aluno> alunos = new ArrayList<>();
+    private void criarAlunos(int quantAlunos, int periodoLetivo, Scanner scanner) {
         for (int i = 0; i < quantAlunos; i++) {
-            System.out.printf("Nome aluno %d: ", i+1);
+            System.out.print("Nome do aluno: ");
             String nome = scanner.nextLine();
             Aluno aluno = new Aluno(nome);
+            System.out.printf("__________________________\n>> Aluno [%s] registrado com sucesso.\n", nome);
             ArrayList<Double> notas = this.criarNotasAluno(periodoLetivo, scanner);
             aluno.setNotas(notas);
-            alunos.add(aluno);
+            this.alunos.add(aluno);
+            this.escola.addAluno(aluno);
         }
-
-        return alunos;
     }
 
     private ArrayList<Double> criarNotasAluno(int periodoLetivo, Scanner scanner) {
@@ -91,8 +149,6 @@ public class Aplicacao {
                 try {
                     System.out.printf("Nota referente ao periodo %s: ", i+1);
                     double nota = scanner.nextDouble();
-                    scanner.nextLine();
-
                     if (nota > 10.0) {
                         nota = 10.0;
                     }
@@ -105,8 +161,11 @@ public class Aplicacao {
                         sucesso = true;
                     }
 
+                    scanner.nextLine();
+
                 } catch (Exception err) {
                     System.out.println("Input inválido. Digite somente números inteiros ou de pontos flutuantes.");
+                    scanner.next();
                 }
             }
         }
