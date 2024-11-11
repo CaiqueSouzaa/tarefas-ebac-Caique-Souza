c2a9f6b5b02a5ea5077efbd85c237d79376927f7
Caique Souza <99214754+CaiqueSouzaa@users.noreply.github.com>
2024-11-11 12:48:08 -0300

    Update sql.sql

--- mod29/sql/sql.sql
+++ mod29/sql/sql.sql
@@ -23,3 +23,27 @@ SELECT * FROM tb_clients;
 
 -- Excluindo um registro da tabela "tb_clients"
 DELETE FROM tb_clients;
+
+-- Crição da tabela "tb_products"
+create table tb_products (
+	id bigint not null,
+	code varchar (50) not null,
+	name varchar(250) not null,
+	description text,
+	created_at timestamp not null default current_timestamp,
+	constraint pk_id_products primary key(id),
+	constraint uq_code_products unique(code)
+);
+
+-- Criação da sequence "sq_id_products"
+create sequence sq_id_products
+start 1
+increment 1
+owned by tb_products.id;
+
+-- Busca na tabela "tb_products"
+SELECT * FROM tb_products;
+
+-- Exclusão dos registros de "tb_products"
+delete from tb_products;
+
